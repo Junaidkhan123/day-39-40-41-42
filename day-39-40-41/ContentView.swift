fbe248159b0d9ade5714f1f25e58526f023a0d07
Muhammad  Junaid <junaid123.jk78@gmail.com>
2020-07-27 16:27:53 +0500

    Initial Commit

---
+++ day-39-40-41/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  day-39-40-41
+//
+//  Created by Jhon Khan on 27/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day-39-40-41/day_39_40_41App.swift
@@ -0,0 +1,17 @@
+//
+//  day_39_40_41App.swift
+//  day-39-40-41
+//
+//  Created by Jhon Khan on 27/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct day_39_40_41App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
